5a637eabde37ddc4978845b6be03f0d06848598d
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-10 19:30:07 +0200

    adding components + routing to them

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import { Routes, Route } from 'react-router'
 import './App.css'
 import Header from "./components/header/Header.jsx"
 import Home from './components/home/Home.jsx'
+import Login from './components/Login/Login.jsx'
+import Register from './components/register/register.jsx'
+import GameCreate from './components/game-create/GameCreate.jsx'
+import GameCatalog from './components/game-catalog/GameCatalog.jsx'
 
 function App() {
     return (
@@ -13,6 +17,10 @@ function App() {
                 <main id="main-content">
                     <Routes>
                         <Route path='/' element={<Home />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                        <Route path='/games/create' element={<GameCreate />} />
+                        <Route path='/games' element={<GameCatalog />} />
                     </Routes>
                 </main>
             </div>
