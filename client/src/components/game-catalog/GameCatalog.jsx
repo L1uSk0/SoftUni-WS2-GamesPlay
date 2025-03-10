86103bebaf282ef9c49dc6664efd404feba58db6
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-10 22:10:25 +0200

    slight upgrade to the game catalog

--- client/src/components/game-catalog/GameCatalog.jsx
+++ client/src/components/game-catalog/GameCatalog.jsx
@@ -24,10 +24,9 @@ export default function GameCatalog() {
             <section id="catalog-page">
                 <h1>All Games</h1>
                 {/* Display div: with information about every game (if any) */}
-                {games.length > 0 ? (
-                    games.map(game => <GameItem key={game._id} {...game} />)
-                ):
-                <h3 className="no-articles">No articles yet</h3>
+                {games.length > 0 
+                ?   games.map(game => <GameItem key={game._id} {...game} />)
+                :   <h3 className="no-articles">No articles yet</h3>
                 }
 
                 {/* Display paragraph: If there is no games  */}
