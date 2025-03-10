0ab6c68cf566feb3ed7ccb7a0b7672a5f65c4a05
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-10 19:23:42 +0200

    add header links and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
-import {Routes,Route} from 'react-router'
+import { Routes, Route } from 'react-router'
 
 import './App.css'
 import Header from "./components/header/Header.jsx"
 import Home from './components/home/Home.jsx'
 
 function App() {
-  return (
-    <>
-      <div id="box">
-        <Header />
+    return (
+        <>
+            <div id="box">
+                <Header />
 
-        <main id="main-content">
-          <Routes>
-            <Route path='/' element={<Home />}/>
-          </Routes>
-        </main>
-      </div>
-    </>
-  )
+                <main id="main-content">
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                    </Routes>
+                </main>
+            </div>
+        </>
+    )
 }
 
 export default App
