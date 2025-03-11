900033e3ce50b9b41449220a64cc80a6693302f5
L1uSk0 <156697398+L1uSk0@users.noreply.github.com>
2025-03-11 22:50:44 +0200

    login and register page

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,8 +1,24 @@
+import { useNavigate } from "react-router";
+import { userService } from "../../services/userServices.js";
+import { Link } from "react-router";
+
 export default function Login() {
+    const navigate = useNavigate();
+
+    const loginAction = async (formData) => {
+        const {email , password} = Object.fromEntries(formData);
+        if(!email || !password){
+            return alert("All fields must be filled")
+        }
+
+        await userService.login({email,password});
+        navigate('/');
+    }
+
     return (
         <>
             <section id="login-page" className="auth">
-                <form id="login">
+                <form id="login" action={loginAction}>
                     <div className="container">
                         <div className="brand-logo" />
                         <h1>Login</h1>
@@ -18,7 +34,7 @@ export default function Login() {
                         <input type="submit" className="btn submit" defaultValue="Login" />
                         <p className="field">
                             <span>
-                                If you don't have profile click <a href="#">here</a>
+                                If you don't have profile click <Link to="/register">here</Link>
                             </span>
                         </p>
                     </div>
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,8 +1,26 @@
+import { useNavigate } from "react-router";
+import { userService } from "../../services/userServices.js";
+import { Link } from "react-router";
+
 export default function Register() {
+    const navigate = useNavigate();
+
+    const registerAction = async (formData) => {
+        const {email , password , 'confirm-password':rePass} = Object.fromEntries(formData);
+        if(!email || !password || !rePass){
+            return alert("All fields must be filled")
+        }
+        if(password !== rePass){
+            return alert("Password and repassword must match !!!");
+        }
+        await userService.register({email,password});
+        navigate('/');
+    }
+
     return (
         <>
             <section id="register-page" className="content auth">
-                <form id="register">
+                <form id="register" action={registerAction}>
                     <div className="container">
                         <div className="brand-logo" />
                         <h1>Register</h1>
@@ -20,7 +38,7 @@ export default function Register() {
                         <input className="btn submit" type="submit" defaultValue="Register" />
                         <p className="field">
                             <span>
-                                If you already have profile click <a href="#">here</a>
+                                If you already have profile click <Link to="/login">here</Link>
                             </span>
                         </p>
                     </div>
